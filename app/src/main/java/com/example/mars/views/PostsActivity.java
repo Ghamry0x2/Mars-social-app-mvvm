b031a65d3b18e32111c235b030ee129b086a96d0
ghamry98 <ghamry98@hotmail.com>
2021-01-07 13:56:13 +0200

    appbar fix
    Signed-off-by: ghamry98 <ghamry98@hotmail.com>

--- app/src/main/java/com/example/mars/views/PostsActivity.java
+++ app/src/main/java/com/example/mars/views/PostsActivity.java
@@ -1,17 +1,12 @@
 package com.example.mars.views;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.example.mars.R;
@@ -53,32 +48,12 @@ public class PostsActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
 
-        LinearLayout linearLayout = new LinearLayout(actionBar.getThemedContext());
-        linearLayout.setOrientation(LinearLayout.HORIZONTAL);
-        ActionBar.LayoutParams layoutParams = new ActionBar.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT
-                | Gravity.CENTER_VERTICAL);
-        linearLayout.setLayoutParams(layoutParams);
-
-        ImageView userAvatar = new ImageView(linearLayout.getContext());
-        userAvatar.setLayoutParams(new LinearLayout.LayoutParams(100, 100));
+        ImageView userAvatar = findViewById(R.id.userAvatar);
         Glide.with(PostsActivity.this)
                 .load(user.avatar)
                 .centerCrop()
                 .circleCrop()
                 .into(userAvatar);
-
-        TextView title = new TextView(linearLayout.getContext());
-        title.setText(getString(R.string.feeds));
-        title.setTextSize(20);
-        title.setTypeface(null, Typeface.BOLD);
-        title.setPadding(50,10, 0, 30);
-        title.setTextColor(Color.WHITE);
-
-        linearLayout.addView(userAvatar);
-        linearLayout.addView(title);
-
-        actionBar.setCustomView(linearLayout);
     }
 
     private void initViewModels() {
